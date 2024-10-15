d7d4c4d32801eaa5c92c52b47e9b316082d3468f
giaphu290 <phutg2000@gmail.com>
2024-10-15 21:58:09 +0700

    SeedData #1

--- PRN - Final Project/Program.cs
+++ PRN - Final Project/Program.cs
@@ -5,6 +5,7 @@ using PRN___Final_Project.MiddleWare;
 using DataAccessObjects;
 using Microsoft.AspNetCore.Identity;
 using System;
+using DataAccessObjects.SeedData;
 
 namespace PRN___Final_Project
 {
@@ -34,6 +35,17 @@ namespace PRN___Final_Project
                 options.LoginPath = "/Account/Login";
                 options.LogoutPath = "/Account/Logout";
                 options.AccessDeniedPath = "/Account/AccessDenied";
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+                options.SlidingExpiration = true;
+                options.Events.OnSigningIn = c =>
+                {
+                    if (c.Properties.IsPersistent)
+                    {
+                        var issued = c.Properties.IssuedUtc ?? DateTimeOffset.UtcNow;
+                        c.Properties.ExpiresUtc = issued.AddDays(16);
+                    }
+                    return Task.FromResult(0);
+                };
 
             });
             // Add services to the container.
@@ -42,7 +54,22 @@ namespace PRN___Final_Project
             builder.Services.ConfigureService(builder.Configuration);
 
             var app = builder.Build();
-
+ 
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var initialiser = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitialiser>();
+                    initialiser.InitialiseAsync().Wait();
+                    initialiser.SeedAsync().Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
