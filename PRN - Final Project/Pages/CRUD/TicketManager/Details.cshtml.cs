91051190ad15883a96fdeb0a8f9a75a95db0067a
ethas <liebeedelvais@gmail.com>
2024-10-15 16:26:36 +0700

    Lag commit

---
+++ PRN - Final Project/Pages/CRUD/TicketManager/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BussinessObjects;
+using DataAccessObjects;
+
+namespace PRN___Final_Project.Pages.CRUD.TicketManager
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly DataAccessObjects.Fall2024DbContext _context;
+
+        public DetailsModel(DataAccessObjects.Fall2024DbContext context)
+        {
+            _context = context;
+        }
+
+        public Ticket Ticket { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticket = await _context.Tickets.FirstOrDefaultAsync(m => m.TicketId == id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Ticket = ticket;
+            }
+            return Page();
+        }
+    }
+}
