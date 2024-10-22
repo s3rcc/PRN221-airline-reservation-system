32d44510b6d43ada040100ae0d4eb406116dc259
s3rcc <s3rcc.9@gmail.com>
2024-10-22 17:00:18 +0700

    statistic

--- Services/Interfaces/IPaymentService.cs
+++ Services/Interfaces/IPaymentService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BussinessObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace Services.Interfaces
 {
     public interface IPaymentService
     {
+        Task<Payment> GetPaymentByUserId(string userId);
+        Task<IEnumerable<Payment>> GetAllPayments();
+        Task<IEnumerable<Payment>> GetPayments(int year);
+        Task<IEnumerable<Payment>> GetPayments(DateTime startDate, DateTime endDate);
+        Task<decimal> GetRevenue();
     }
 }
