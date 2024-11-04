0f812c7e7484b21bdd6622226d6690c85dd45aec
s3rcc <s3rcc.9@gmail.com>
2024-11-04 15:24:33 +0700

    Fix flight

--- Services/Interfaces/IFlightService.cs
+++ Services/Interfaces/IFlightService.cs
@@ -8,9 +8,9 @@ namespace Services.Interfaces
         Task<IEnumerable<Flight>> GetAllAvailableFlightsAsync();
         Task<Flight> GetFlightByIdAsync(int id);
         Task<Flight> GetReturnFlightByIdAsync(int? id);
-        Task AddFlightAsync(Flight flight);
-        Task UpdateFlightAsync(Flight flight);
-        Task DeleteFlightAsync(int id);
+        Task<string> AddFlightAsync(Flight flight);
+        Task<string> UpdateFlightAsync(Flight flight);
+        Task<string> DeleteFlightAsync(int id);
         Task<IEnumerable<Flight>> FilterFlightsAsync(int originId, int destinationId, DateTime departureTime, int totalPassengers);
         Task<IEnumerable<Flight>> GetFlightsByYear(int year);
         Task<IEnumerable<Flight>> GetFlightsByMonth(DateTime startDate, DateTime endDate);
